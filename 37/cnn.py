9de07569f0c50de6a482a5d10b4d810386f590b6
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-10-01 22:33:42 +0800

    refactored some files

--- 37/cnn.py
+++ 37/cnn.py
@@ -11,13 +11,13 @@ train_datagen = ImageDataGenerator(
     rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
 )
 training_set = train_datagen.flow_from_directory(
-    "../dataset/training_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+    "./dataset/training_set", target_size=(64, 64), batch_size=32, class_mode="binary"
 )
 
 # Preprocessing the Test set
 test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 test_set = test_datagen.flow_from_directory(
-    "../dataset/test_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+    "./dataset/test_set", target_size=(64, 64), batch_size=32, class_mode="binary"
 )
 
 # Initialising the CNN
@@ -54,7 +54,7 @@ cnn.fit(x=training_set, validation_data=test_set, epochs=32)
 
 # Making a single prediction (dog1)
 test_image = load_img(
-    "../dataset/single_prediction/cat_or_dog_1.jpg", target_size=(64, 64)
+    "./dataset/single_prediction/cat_or_dog_1.jpg", target_size=(64, 64)
 )
 test_image = img_to_array(test_image)
 test_image = expand_dims(test_image, axis=0)
--- 44/xg_boost.py
+++ 44/xg_boost.py
@@ -2,30 +2,33 @@
 XGBoost
 """
 # Importing the libraries
-from pandas import read_csv
+from warnings import filterwarnings
+from os.path import dirname
+from pandas import read_csv, DataFrame
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 # Importing the dataset
-dataset = read_csv("Data.csv")
+path = dirname(__file__)
+filterwarnings("ignore", category=UserWarning)
+dataset = DataFrame(read_csv(rf"{path}\Data.csv"))
 X = dataset.iloc[:, :-1].values
-dataset = read_csv("Data.csv")
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=0
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print(X_train)
+print(y_train)
 
 # Training XGBoost on the Training set
-classifier = XGBClassifier()
+classifier = XGBClassifier(eval_metric="mlogloss")
 classifier.fit(X_train, y_train)
 
 # Making the Confusion Matrix
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
-print("confusion_matrix:", cm)
+print("confusion_matrix:\n", cm)
 acc = accuracy_score(y_test, y_pred)
 print("accuracy_score", acc)
 
